7a97974c50ddd6c61512db4cca206498a9c6872c
tellessa <stephentelles@yahoo.com>
2022-06-11 15:00:37 -0700

    Add command line input for choosing which graph to make.

--- plt_graphs_and_charts.py
+++ plt_graphs_and_charts.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 
 def draw_a_line():
@@ -97,7 +98,33 @@ def draw_2_plots_with_title_and_super_title():
     plt.show()
 
 
+QUESTION = "What would you like to do? Only enter the numeric value"
+OPTIONS = {
+    "1: draw a line": draw_a_line,
+    "2: draw two points": draw_two_points,
+    "3: draw a line with more points": draw_a_line_with_more_points,
+    "4: draw 2 plots with title and super title": draw_2_plots_with_title_and_super_title,
+    "5: draw 6 plots": draw_6_plots,
+    "6: exit the program": sys.exit
+}
+
+
+def main():
+    while True:
+        print(QUESTION)
+        for option in OPTIONS.keys():
+            # print(f"{option}.__qualname__")
+            print(f"{option}")
+        user_choice_index = input()
+        for choice in OPTIONS:
+            if user_choice_index + ":" in choice:
+                match = choice
+                break
+
+        function = OPTIONS[match]
+        function()  # execute the code for the user's choice
+        # ask again
+
+
 if __name__ == "__main__":
-    # draw_a_line_with_more_points()
-    # draw_line_using_default_x_points()
-    draw_2_plots_with_title_and_super_title()
+    main()
