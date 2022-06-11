2655e7684cddec8a11a8970e0063987c366db8d9
tellessa <stephentelles@yahoo.com>
2022-06-11 14:17:46 -0700

    Add docstrings to functions

--- plt_graphs_and_charts.py
+++ plt_graphs_and_charts.py
@@ -1,30 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Draw a line in a diagram from position (0,0) to position (6,250):
-
 
 def draw_a_line():
+    """Draw a line in a diagram from position (0,0) to position (6,250)"""
     xpoints = np.array([0, 6])
     ypoints = np.array([0, 250])
 
     plt.plot(xpoints, ypoints)
     plt.show()
 
-# Draw two points in the diagram, one at position (1, 3) and one in position (8, 10):
-
 
 def draw_two_points():
+    """Draw two points in the diagram, one at position (1, 3) and one in position (8, 10)"""
     xpoints = np.array([1, 8])
     ypoints = np.array([3, 10])
 
     plt.plot(xpoints, ypoints, 'o')
     plt.show()
 
-# Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10):
-
 
 def draw_a_line_with_more_points():
+    """Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10)"""
     xpoints = np.array([1, 2, 6, 8])
     ypoints = np.array([3, 8, 1, 10])
 
