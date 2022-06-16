8e9e2e9bb4bde82bcaca255efc4971f55243d400
tellessa <stephentelles@yahoo.com>
2022-06-16 07:28:28 -0700

    add create_scatter_plot, create_bar_chart_with_linkedin_data, and read_csv.

--- plt_graphs_and_charts.py
+++ plt_graphs_and_charts.py
@@ -1,6 +1,9 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+
+import pandas as pd
 
 
 def draw_a_line():
@@ -98,6 +101,36 @@ def draw_2_plots_with_title_and_super_title():
     plt.show()
 
 
+def create_scatter_plot():
+    """Scatter plots are used to observe relationships between variables."""
+    x = np.array([5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6])
+    y = np.array([99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86])
+
+    plt.scatter(x, y)
+    plt.show()
+
+
+def create_barchart_with_linkedin_data():
+    search_results_by_keyword_dataframe, dataframe_info_for_title = read_csv()
+    job_postings_with_that_keyword = search_results_by_keyword_dataframe["results"]
+    # data = [23, 85, 72, 43, 52]
+    keywords = search_results_by_keyword_dataframe["keyword"]
+    # labels = ['A', 'B', 'C', 'D', 'E']
+    plt.xticks(range(len(job_postings_with_that_keyword)), keywords)
+    plt.xlabel("Keyword")
+    plt.ylabel("Number of Searches")
+    plt.title(dataframe_info_for_title)
+    plt.bar(range(len(job_postings_with_that_keyword)), job_postings_with_that_keyword)
+    plt.show()
+
+
+def read_csv():
+    search_results_by_keyword_dataframe = pd.read_csv("linkedin_keywords.csv")
+    dataframe_info = "Linkedin job search results by keyword:\n"
+    print(dataframe_info, search_results_by_keyword_dataframe.head())
+    return search_results_by_keyword_dataframe, dataframe_info
+
+
 QUESTION = "What would you like to do? Only enter the numeric value"
 OPTIONS = {
     "1: draw a line": draw_a_line,
@@ -105,7 +138,10 @@ OPTIONS = {
     "3: draw a line with more points": draw_a_line_with_more_points,
     "4: draw 2 plots with title and super title": draw_2_plots_with_title_and_super_title,
     "5: draw 6 plots": draw_6_plots,
-    "6: exit the program": sys.exit
+    "6: load the csv data": read_csv,
+    "7: draw a scatterplot": create_scatter_plot,
+    "8: draw a barchart using the LinkedIn search_data": create_barchart_with_linkedin_data,
+    "9: exit the program": sys.exit
 }
 
 
