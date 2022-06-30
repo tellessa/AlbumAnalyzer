25112a3729076bc458ce0d60097bab77ee4f7b99
tellessa <76462081+tellessa@users.noreply.github.com>
2022-06-30 10:01:41 -0700

    Find initial working solution for roman to int

--- leetcode_solutions/roman_to_int.py
+++ leetcode_solutions/roman_to_int.py
@@ -1,9 +1,90 @@
 import unittest
 
+# There are six instances where subtraction is used:
+#     I can be placed before V (5) and X (10) to make 4 and 9.
+#     X can be placed before L (50) and C (100) to make 40 and 90.
+#     C can be placed before D (500) and M (1000) to make 400 and 900.
+#     Given a roman numeral, convert it to an integer.
+
+roman_3999 = "MMMCMXCIX"  # first guess right!
+
+# constraints
+# --> come back to these constraints to narrow the general answer
+# 1 <= s.length <= 15
+# s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+# --> We don't have to handle absurd cases
+# It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+# --> 3999 in roman numerals:
+
+# Roman numerals are usually written largest to smallest from left to right.
+#     However, the numeral for four is not IIII.
+#     Instead, the number four is written as IV.
+#     Because the one is before the five we subtract it making four.
+#     The same principle applies to the number nine, which is written as IX.
+
+
+# Example 1:
+
+# Input: s = "III"
+# Output: 3
+# Explanation: III = 3.
+# Example 2:
+
+# Input: s = "LVIII"
+# Output: 58
+# Explanation: L = 50, V= 5, III = 3.
+# Example 3:
+
+# Input: s = "MCMXCIV"
+# Output: 1994
+# Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+cases_where_subtraction_used = {
+    "IV": 4,
+    "IX": 9,
+    "XL": 40,
+    "XC": 90,
+    "CD": 400,
+    "CM": 900
+}
+
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+
+        # Set up the basic mapping
+        symbols_to_values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        # convert to list since strings are immutable
+        list_of_letters = [char for char in s]
+        arabic_number = 0
+        while len(list_of_letters):
+            if len(list_of_letters) == 1:
+                letter = list_of_letters.pop()
+                arabic_number += symbols_to_values[letter]
+            else:
+                # Get the character before the character at the
+                letter = list_of_letters.pop()
+                letter_before = list_of_letters[-1]
+                if symbols_to_values[letter] > symbols_to_values[letter_before]:
+                    # Pop an additional letter if it needs to be subtracted from its partner
+                    # rather than summed with total
+                    letter_before = list_of_letters.pop()
+                    unit = letter_before + letter
+                    value = cases_where_subtraction_used[unit]
+                    arabic_number += value
+                else:
+                    letter_value = symbols_to_values[letter]
+                    arabic_number += letter_value
+
+        return arabic_number
 
 
 class TestSolution(unittest.TestCase):
@@ -13,8 +94,16 @@ class TestSolution(unittest.TestCase):
 
     def test_romanToInt_58(self, s: str) -> int:
         solution = Solution()
-        assert 3 == solution.romanToInt("III")
+        assert 58 == solution.romanToInt("LVIII")
 
     def test_romanToInt_1994(self, s: str) -> int:
         solution = Solution()
-        assert 3 == solution.romanToInt("III")
+        assert 1994 == solution.romanToInt("MCMXCIV")
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.romanToInt("XXXIV"))
+    print(solution.romanToInt("III"))
+    print(solution.romanToInt("LVIII"))
+    print(solution.romanToInt("MCMXCIV"))
