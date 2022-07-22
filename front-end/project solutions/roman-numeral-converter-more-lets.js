bd16ca981baf64a5a619aa4fc4e9d6b3a01153ea
tellessa <76462081+tellessa@users.noreply.github.com>
2022-07-22 16:18:33 -0700

    Add comments at the top describing the general pattern to convert each arabic digit to a numeral

--- front-end/project solutions/roman-numeral-converter-more-lets.js
+++ front-end/project solutions/roman-numeral-converter-more-lets.js
@@ -1,48 +1,36 @@
+
+/* If arabic is in range(0,3 or 6,8 (inclusive), the substring's end character will be
+that order of magnitude's floor-numeral
+If arabic is in range (4, 5) it will end in the the mid-range numeral (V, L, or D)
+If arabic is in range (5, 8) inclusive, it needs the mid-range numeral at the start of the substring
+If arabic is 9, it needs the floor-numeral followed by the ceiling numeral */
+
 function convertToRoman(num) {
     let numAsString = num.toString();
     let length = numAsString.length;
-    let romanString;
     let onesPlaceArabic = numAsString[numAsString.length -1];
-    let onesPlaceSubstring = convertDigit(onesPlaceArabic, "ones");
-    romanString = onesPlaceSubstring;
+    // let romanString = convertDigit(onesPlaceArabic, "ones");
+    const onesConversion = {
+        "0": "",
+        "1": "I",
+        "2": "II",
+        "3": "III",
+        "4": "IV",
+        "5": "V",
+        "6": "VI",
+        "7": "VII",
+        "8": "VIII",
+        "9": "IX"
+    };
+    if (onesPlaceArabic === "0") {
+        let romanString = "";
+    } else {
+        romanString = onesConversion[onesPlaceArabic];
+    }
     if (length >= 2) {
         let tensPlaceArabic = numAsString[numAsString.length -2];
-        let tensPlaceSubstring = convertDigit(tensPlaceArabic, "tens")
-        romanString = tensPlaceSubstring + romanString;
-    }
-    if (length >= 3) {
-        let hundredsPlaceArabic = numAsString[numAsString.length -3];
-        let hundredsPlaceSubstring = convertDigit(hundredsPlaceArabic, "hundreds")
-        romanString = hundredsPlaceSubstring + romanString;
-    }
-    if (length === 4) {
-        let thousandsPlaceArabic = numAsString[numAsString.length -4];
-        let thousandsPlaceSubstring = convertDigit(thousandsPlaceArabic, "thousands")
-        romanString = thousandsPlaceSubstring + romanString;
-    }
-    return romanString;
-}
-
-function convertDigit(numAsString, place) {
-    let numeral;
-    if (numAsString === "0") {
-        return "";
-    }
-    if (place === "ones") {
-        const onesConversion = {
-            "1": "I",
-            "2": "II",
-            "3": "III",
-            "4": "IV",
-            "5": "V",
-            "6": "VI",
-            "7": "VII",
-            "8": "VIII",
-            "9": "IX"
-        };
-        numeral = onesConversion[numAsString];
-    } else if (place === "tens") {
         const tensConversion = {
+            "0": "",
             "1": "X",
             "2": "XX",
             "3": "XXX",
@@ -53,9 +41,13 @@ function convertDigit(numAsString, place) {
             "8": "LXXX",
             "9": "XC"
         }
-        numeral = tensConversion[numAsString]
-    } else if (place === "hundreds") {
+        let tensPlaceSubstring = tensConversion[tensPlaceArabic];
+        romanString = tensPlaceSubstring + romanString;
+    }
+    if (length >= 3) {
+        let hundredsPlaceArabic = numAsString[numAsString.length -3];
         const hundredsConversion = {
+            "0": "",
             "1": "C",
             "2": "CC",
             "3": "CCC",
@@ -66,17 +58,22 @@ function convertDigit(numAsString, place) {
             "8": "DCCC",
             "9": "CM"
         }
-        numeral = hundredsConversion[numAsString]
-    } else if (place === "thousands") {
+        let hundredsPlaceSubstring = hundredsConversion[hundredsPlaceArabic];
+        romanString = hundredsPlaceSubstring + romanString;
+        }
+    }
+    if (length === 4) {
+        let thousandsPlaceArabic = numAsString[numAsString.length -4];
         const thousandsConversion = {
+            "0": "",
             "1": "M",
             "2": "MM",
             "3": "MMM",
         }
-        numeral = thousandsConversion[numAsString]
-    }
-    return numeral;
-}
+        let thousandsPlaceSubstring = thousandsConversion[thousandsPlaceArabic];
+        romanString = thousandsPlaceSubstring + romanString;
+        }
+        return romanString;
 
 // console.log(convertToRoman(120));
 // console.log(convertToRoman(400));
