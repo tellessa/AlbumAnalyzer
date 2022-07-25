5c96b0079b176b748584bb66699688440b6cdb1b
tellessa <76462081+tellessa@users.noreply.github.com>
2022-07-25 08:11:58 -0700

    Add a program.html and a program.js and style.css to the front-end folder.

---
+++ front-end/program.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 3; i++) {
+document.writeln("Hello, world!");
+document.writeln("<br/>");
+}
+
+// Function.prototype.method = function (name, func) {
+//     this.prototype[name] = func;
+//     return this;
+// };
