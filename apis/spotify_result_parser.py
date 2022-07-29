23d74add8a54bcd78c654048cd3ad89c64398781
tellessa <stephentelles@yahoo.com>
2022-07-29 08:26:20 -0700

    Get whole album csv. #46

--- apis/spotify_result_parser.py
+++ apis/spotify_result_parser.py
@@ -3,14 +3,6 @@ import json
 
 import pandas as pd
 
-# is_valid = os.path.isfile("../switchfoot_results/track 1.json")
-in_path = "c:\\Users\\Stephen's PC\\Desktop\\desktopBin\\Programming resources\\data_engineering\\django_data_engineering\\track 1.json"
-# path = ".\\track 1.json"
-is_valid = os.path.isfile(in_path)
-# how do we load json from a file with json library?
-
-# Opening JSON file
-
 
 def read_json_with_json(path):
     with open(path) as f:
@@ -20,17 +12,35 @@ def read_json_with_json(path):
         track_1_json = json.load(f)
     return track_1_json
 
-# track_1_json = read_json_with_json(path)
 
+def get_first_track_and_schema(track_index):
+    in_path = f"django_data_engineering\\switchfoot_results\\track {track_index}.json"
+    out_path = f"django_data_engineering\\csvs\\track {track_index}.csv"
+# relative path
+# path = ".\\track 1.json"
+    track_df = pd.read_json(in_path)
+    track_df.to_csv(out_path)
+    return track_df
+
+
+first_track_df = get_first_track_and_schema(1)
+track_index = 2
+for index in range(2, 15):
+    # in_path = f"c:\\Users\\Stephen's PC\\Desktop\\desktopBin\\Programming resources\\data_engineering\\django_data_engineering\\switchfoot_results\\track {track_index}.json"
+    in_path = f"django_data_engineering\\switchfoot_results\\track {track_index}.json"
+    # relative path
+    # path = ".\\track 1.json"
+    is_valid = os.path.isfile(in_path)
+    current_track_df = pd.read_json(in_path)
+    first_track_df = pd.concat([first_track_df, current_track_df])
+    track_index += 1
+# out_path = f"django_data_engineering\\csvs\\track {track_index}.csv"
+out_path = "django_data_engineering\\csvs\\album.csv"
+first_track_df.to_csv(out_path)
 #  working til here
 
 # absolute path
 # out_path = "c:\\Users\\Stephen's PC\\Desktop\\desktopBin\\Programming resources\\data_engineering\\django_data_engineering\\track 1.csv"
 
-
-track_1_df = pd.read_json(in_path)
-# relative path
-out_path = "django_data_engineering\\track 1 b.csv"
-track_1_df.to_csv(out_path)
 # print(track_1_df)
 # print(track_1_df.to_string())
