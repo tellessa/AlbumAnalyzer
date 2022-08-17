bd2078f27ec79dd7de84018c0befe867f0dae106
tellessa <76462081+tellessa@users.noreply.github.com>
2022-08-17 12:57:55 -0700

    Create wherefore-art-thou.js
    added unsolved wherefore-art-thou

---
+++ front-end/algorithm-scripting/intermediate/wherefore-art-thou.js
@@ -0,0 +1,17 @@
+function whatIsInAName(collection, source) {
+  const arr = [];
+  // Only change code below this line
+
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName(
+  [
+  { first: "Romeo", last: "Montague" }, 
+  { first: "Mercutio", last: null }, 
+  { first: "Tybalt", last: "Capulet" }
+  ], 
+  { last: "Capulet" }
+  );
