f1f85a47e988350295ebbc701c00c588b9367faa
livingoutthebeatitudes <111623053+livingoutthebeatitudes@users.noreply.github.com>
2022-08-26 10:03:08 -0700

    Update missing-letters.js

--- front-end/algorithm-scripting/intermediate/missing-letters.js
+++ front-end/algorithm-scripting/intermediate/missing-letters.js
@@ -4,37 +4,45 @@ function fearNotLetter(str) {
   let lastLetter;
   let goldenRange;
   firstLetter = str[0];
-  lastLetter = str[str.length - 1];
-  console.log(firstLetter);
-  console.log(lastLetter);
   // find last letter
+  lastLetter = str[str.length - 1];
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let lastLetterAlphabetIndex = alphabet.indexOf(lastLetter);
+  let firstLetterAlphabetIndex = alphabet.indexOf(firstLetter);
   // find golden range between first and last letters
+  goldenRange = alphaRange(firstLetterAlphabetIndex, lastLetterAlphabetIndex);
+  let strAsList = [...str]
+  let letter;
+  for (let i = 0; i < goldenRange.length; i++) {
+    letter = goldenRange[i];
+    if (strAsList.indexOf(letter) === -1) {
+      console.log(`${goldenRange[i]} is not in strAsList`);
+      return letter;
+    }
+  }
+  return undefined;
   // iterate through golden range and if str.indexOf(golden_range[i]) === -1
   //    return golden_range[i]
   return str;
 }
 
-function letterRange() {
-  const letters = 'abcdefghijklmnopqrstuvwxyz';
-  var randomNum = Math.round(Math.random() * 26);
-  var randomLetter = letters.charAt(randomNum);
-  return completeLetterRange;
-};
+function range(start, stop, step) {
+    var result = [start];
+    var next = start;
+    while (next < stop) {
+        result.push(next += step || 1);
+    }
+    return result;
+}
 
-// Function to check letters and numbers
-// function alphanumeric(inputtxt)
-// {
-//  var letterNumber = /^[a-z]+$/;
-//  if((inputtxt.value.match(letterNumber)) 
-//   {
-//    return true;
-//   }
-// else
-//   { 
-//    alert("message"); 
-//    return false; 
-//   }
-//   }
-
+function alphaRange(start, stop, step){
+    //Assume starting at 0
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    var numRange = range(start, stop, step);
+    var result = numRange.map(el => {
+        return alphabet[el % 26];
+    });
+    return result;
+}
 
 fearNotLetter("abce");
