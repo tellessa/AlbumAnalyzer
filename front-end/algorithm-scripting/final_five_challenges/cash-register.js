1d0914de78f5bf69bcf3e13e1cdbf15c09a9febc
tellessa <stephentelles@yahoo.com>
2022-09-24 11:33:52 -0700

    list the possible types of returns from getChangeActual

--- front-end/algorithm-scripting/final_five_challenges/cash-register.js
+++ front-end/algorithm-scripting/final_five_challenges/cash-register.js
@@ -17,7 +17,7 @@ const DENOMINATION_VALUES = {
 function checkCashRegister(price, cash, cid) {
     let changeDue = cash - price;
     let statusAndChange = {};
-    
+
 
     let [changeActualValue, changeActualArray] = getChangeActual(changeDue, cid);
 
@@ -41,11 +41,11 @@ function checkCashRegister(price, cash, cid) {
     // 50 cents can be
     // one quarter, 5 nickels
     // quarter, 4 nickels, 5 pennies
-    // algorithm outline: 
+    // algorithm outline:
     // 1. always prefer a higher denomination
     // set up a mapping to show the unitValue of each denomination- done
     // Disregard denominations that are in themselves greater than the required change
-    // 
+    //
 
     // Get the total value of cash in drawer
     // compare the change due to the total value of cash in drawer
@@ -56,7 +56,7 @@ function checkCashRegister(price, cash, cid) {
     // INSUFFICIENT_FUNDS
     // c. cidValue > changeDue
     // OPEN
-    
+
 
     return statusAndChange;
     }
@@ -64,6 +64,10 @@ function checkCashRegister(price, cash, cid) {
 getChangeActual = (changeDue, cid) => {
 
     // TODO: modify this function so that we actually decrement from cid as we try to find the ideal change
+    // three outcomes:
+    // 1. an empty list
+    // 2. just return cid if it matches the amount of change due
+    // 3. The change, sorted in high to low order
     let changeActualValue = 0;
     let changeActualArray = [];
     let cidDenomArray;
@@ -82,7 +86,7 @@ getChangeActual = (changeDue, cid) => {
         if ((denom_value < changeDue) && is_key_present_in_cid) {
             let changeDenomArray = [key, 0]
             let cidDenomValue = cidDenomArray[1];
-            // currentChangeActualValue = 
+            // currentChangeActualValue =
 
             while (changeActualValue + denom_value <= changeDue && cidDenomValue > 0) {
                 changeActualValue += denom_value;
@@ -109,7 +113,7 @@ getValue = (cid) => {
         let denominationValue = denomination[1]
         totalValue += denominationValue
     }
-    
+
     // let rounded = Math.round(totalValue)
     let rounded = roundOff(totalValue, 2)
     return rounded
@@ -133,7 +137,7 @@ console.log(test0)
 let test1 = checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 console.log(test1)
 
-// should return 
+// should return
 
 // {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
 
@@ -141,7 +145,7 @@ console.log(test1)
 let test2 = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 console.log(test2)
 
-// should return 
+// should return
 
 // {status: "INSUFFICIENT_FUNDS", change: []}
 
@@ -152,7 +156,7 @@ console.log(test2)
 let test3 = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 console.log(test3)
 
-// should return 
+// should return
 
 // {status: "INSUFFICIENT_FUNDS", change: []}
 
@@ -160,6 +164,6 @@ console.log(test3)
 let test4 = checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
 console.log(test4)
 
-// should return 
+// should return
 
 // {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
