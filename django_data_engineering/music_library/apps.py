d33ffa7afa4aeac43aff19da0ad3994bf5d665b9
tellessa <76462081+tellessa@users.noreply.github.com>
2023-06-29 13:59:45 -0700

    Remove non-portfolio projects

--- django_data_engineering/music_library/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MusicLibraryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'music_library'
--- front-end/program.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i < 3; i++) {
-document.writeln("Hello, world!");
-document.writeln("<br/>");
-}
-
-// Function.prototype.method = function (name, func) {
-//     this.prototype[name] = func;
-//     return this;
-// };
