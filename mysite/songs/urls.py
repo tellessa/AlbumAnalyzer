f0f724c6b0b5e4e8b60587fde933ca49b6bb2f5c
tellessa <76462081+tellessa@users.noreply.github.com>
2025-07-09 14:18:51 -0700

    Adds the LLMChat URL

--- mysite/songs/urls.py
+++ mysite/songs/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 app_name = 'songs'
 urlpatterns = [
     path('', views.MainView.as_view(), name='all'),
+    path('generate_text', views.generate_text_view, name='generate_text_name'),
     path('interactive', views.interactive_python_concept, name='interactive_python_concept_name'),
     path('about', views.about, name='about_name'),
 
